fde377a11f4227d441f4e50b11b8608877fc8629
dbslm31 <104209863+dbslm31@users.noreply.github.com>
2024-10-28 15:40:36 +0100

    Feat : password encryption

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
+import * as bcrypt from 'bcrypt';
 import { User } from './user.model';
 
 @Injectable()
@@ -7,7 +8,17 @@ export class UserService {
     constructor(@InjectModel(User) private userModel: typeof User) { }
 
     async createUser(username: string, email: string, password: string): Promise<User> {
-        return this.userModel.create({ username, email, password });
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+        return this.userModel.create({ username, email, password: hashedPassword });
+    }
+
+    async validateUser(email: string, password: string): Promise<User | null> {
+        const user = await this.userModel.findOne({ where: { email } });
+        if (user && await bcrypt.compare(password, user.password)) {
+            return user;
+        }
+        return null;
     }
 
     async findAll(): Promise<User[]> {
