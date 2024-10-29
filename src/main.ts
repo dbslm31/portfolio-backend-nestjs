883675b559c923832a8d6692e0d3adc00f604d5d
dbslm31 <104209863+dbslm31@users.noreply.github.com>
2024-10-29 20:23:30 +0100

    feat: CORS configuration

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,18 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const sequelize = app.get(Sequelize);
+
   await sequelize.sync();
+
   app.use(cookieParser());
+
+
+  app.enableCors({
+    origin: ['*'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
